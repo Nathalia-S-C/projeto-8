be47b157adba929dfaede8e43c29103f9b8ab9d5
Oem <natysantana1949@gmail.com>
2023-10-03 11:10:53 -0300

    second commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var n = parseInt(prompt('Digite um número inteiro:'));
+
+if(n % 2 === 0 && n < 100 && n > 0){
+    alert('O número atende aos critérios')
+}else{
+    alert('O número não atende aos critérios')
+}
